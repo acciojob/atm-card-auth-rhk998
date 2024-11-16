3839a3072942de5e1a2731bd9568ff793da77758
rhk998 <hemanthk.998r@gmail.com>
2024-11-16 12:57:11 +0530

    noon2

--- src/main/java/com/driver/InvalidPinException.java
+++ src/main/java/com/driver/InvalidPinException.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class InvalidPinException extends Throwable {
 	public InvalidPinException(String message) {
-	 // your code goes here
+
         super(message);
     }
 }
